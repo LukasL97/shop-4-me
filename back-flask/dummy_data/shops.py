7fdd467f1cb5137ef6c6a81c10e64536be58aede
Asabeneh <asabeneh@gmail.com>
2020-09-28 11:24:06 +0300

    MongoDB_URI has been changed

---
+++ back-flask/dummy_data/shops.py
@@ -0,0 +1,452 @@
+shops = [
+  {
+    "id":"poZBIfzf5LsbVfqrqsNRzTws",
+    "address":{
+      "zip":"52473",
+      "streetName":"Schuster Rest",
+      "coordinates":{"lat":"-15.0621","lng":"133.0752"}
+    },
+    "owner":"Kobe"
+  },
+  {
+    "id":"9PQfo4BlhaqSGJgJn19W3vPr",
+    "address":{
+      "zip":"23871",
+      "streetName":"Orn Common",
+      "coordinates":{"lat":"-0.7304","lng":"-53.6500"}
+    },
+    "owner":"Brianne"
+  },
+  {
+    "id":"zTT0PF4QHibzBTfyilX2c3tR",
+    "address":{
+      "zip":"75960",
+      "streetName":"Marty Crest",
+      "coordinates":{"lat":"61.2262","lng":"129.5931"}
+    },
+    "owner":"Jordon"
+  },
+  {
+    "id":"dMsHKLMJeOTFbZriRfWzOMBZ",
+    "address":{
+      "zip":"50569",
+      "streetName":"Schultz Mount",
+      "coordinates":{"lat":"-87.6767","lng":"-99.5116"}
+    },
+    "owner":"Keanu"
+  },
+  {
+    "id":"YOh4A121gKDBjZXPbEYoY7p4",
+    "address":{
+      "zip":"22514",
+      "streetName":"Chet Greens",
+      "coordinates":{"lat":"18.6109","lng":"102.5322"}
+    },
+    "owner":"Columbus"
+  },
+  {
+    "id":"Fz3qcMDicrNqlZSl7ec6Ce7I",
+    "address":{
+      "zip":"91538-1003",
+      "streetName":"Beier Knolls",
+      "coordinates":{"lat":"-87.8507","lng":"-123.8257"}
+    },
+    "owner":"Lela"
+  },
+  {
+    "id":"RdUjzBLA8LVmAz0REvNByrGK",
+    "address":{
+      "zip":"11674",
+      "streetName":"Mills Plain",
+      "coordinates":{"lat":"60.2645","lng":"-23.6761"}
+    },
+    "owner":"Mable"
+  },
+  {
+    "id":"wN0Lk5nKVsDzPptBG8K6GAgb",
+    "address":{
+      "zip":"65555-6091",
+      "streetName":"Fay Route",
+      "coordinates":{"lat":"-74.6473","lng":"132.0611"}
+    },
+    "owner":"Damien"
+  },
+  {
+    "id":"FpzLAP3VdjKUlXHgW40Mk2FL",
+    "address":{
+      "zip":"48096-0565",
+      "streetName":"West Port",
+      "coordinates":{"lat":"-70.1761","lng":"-94.3505"}
+    },
+    "owner":"Ryley"
+  },
+  {
+    "id":"On7p4R4Q18IeY38i3Qj37KPj",
+    "address":{
+      "zip":"66813",
+      "streetName":"Ervin Overpass",
+      "coordinates":{"lat":"-85.2888","lng":"19.1814"}
+    },
+    "owner":"Ayla"
+  },
+  {
+    "id":"QdRiQhH37aoK4HVkuy0z2nzq",
+    "address":{
+      "zip":"86353",
+      "streetName":"Meaghan Valley",
+      "coordinates":{"lat":"-79.1633","lng":"-23.4245"}
+    },
+    "owner":"Easton"
+  },
+  {
+    "id":"tAUh864f5tpprd5StLMfhzWt",
+    "address":{
+      "zip":"68271",
+      "streetName":"Jacobs Trail",
+      "coordinates":{"lat":"-48.0254","lng":"-103.9013"}
+    },
+    "owner":"Deangelo"
+  },
+  {
+    "id":"9E7kqKCWMCGTsFI8SYti3ia0",
+    "address":{
+      "zip":"00987",
+      "streetName":"Gerard Skyway",
+      "coordinates":{"lat":"55.8854","lng":"-24.2645"}
+    },
+    "owner":"Coty"
+  },
+  {
+    "id":"ouzXBAP2GoMOuC9bYJGEZRgf",
+    "address":{
+      "zip":"77952",
+      "streetName":"Zulauf Knoll",
+      "coordinates":{"lat":"-48.4750","lng":"57.3287"}
+    },
+    "owner":"Terry"
+  },
+  {
+    "id":"qgcyeD6LbkdHW8kXk1C8Onzf",
+    "address":{
+      "zip":"44922-7164",
+      "streetName":"Gillian Common",
+      "coordinates":{"lat":"-32.7208","lng":"53.4549"}
+    },
+    "owner":"Dimitri"
+  },
+  {
+    "id":"ov1oZeUV7APz2kGKpBli050H",
+    "address":{
+      "zip":"36138-9035",
+      "streetName":"Destin Mission",
+      "coordinates":{"lat":"-12.5345","lng":"-161.1965"}
+    },
+    "owner":"Royce"
+  },
+  {
+    "id":"mzHk5JHBMQsZHGM7faWCWTFJ",
+    "address":{
+      "zip":"24961-2896",
+      "streetName":"Ankunding Street",
+      "coordinates":{"lat":"70.8460","lng":"3.0146"}
+    },
+    "owner":"Iva"
+  },
+  {
+    "id":"OcSMBnOnglQvUAbazuDXg9L0",
+    "address":{
+      "zip":"38012",
+      "streetName":"Mayer Rest",
+      "coordinates":{"lat":"43.7867","lng":"179.8067"}
+    },
+    "owner":"Miller"
+  },
+  {
+    "id":"EPF6BiJ4fXHPiztzm1iWGcL4",
+    "address":{
+      "zip":"73250-0208",
+      "streetName":"Arnoldo Stream",
+      "coordinates":{"lat":"-21.4965","lng":"-172.3786"}
+    },
+    "owner":"Lorenza"
+  },
+  {
+    "id":"PI49HsDuvvzJLelTBJi6pqcJ",
+    "address":{
+      "zip":"90927-4654",
+      "streetName":"Greenholt Springs",
+      "coordinates":{"lat":"26.8294","lng":"123.0442"}
+    },
+    "owner":"Angie"
+  },
+  {
+    "id":"Bzj3WalMSbQrLSOymMoCvqIt",
+    "address":{
+      "zip":"06653",
+      "streetName":"Cartwright Motorway",
+      "coordinates":{"lat":"69.2310","lng":"34.8607"}
+    },
+    "owner":"Isabella"
+  },
+  {
+    "id":"QGAihqifumGDyb1rDDJS0EY9",
+    "address":{
+      "zip":"73504",
+      "streetName":"Thad Mews",
+      "coordinates":{"lat":"13.6204","lng":"-14.4780"}
+    },
+    "owner":"Lauryn"
+  },
+  {
+    "id":"RzB3L4VgmB1ijmbchK9jceBU",
+    "address":{
+      "zip":"53789-7331",
+      "streetName":"Terrence Port",
+      "coordinates":{"lat":"13.3414","lng":"-161.3232"}
+    },
+    "owner":"Savanah"
+  },
+  {
+    "id":"A8eWb0Xbme8Z6DwHbWvXy5rp",
+    "address":{
+      "zip":"96878-7001",
+      "streetName":"Jude Mills",
+      "coordinates":{"lat":"29.6242","lng":"27.3843"}
+    },
+    "owner":"Tony"
+  },
+  {
+    "id":"kYDMM9WARpeLav3B8VjbkYC8",
+    "address":{
+      "zip":"28714-4497",
+      "streetName":"Watsica Oval",
+      "coordinates":{"lat":"32.5036","lng":"-7.9804"}
+    },
+    "owner":"Reed"
+  },
+  {
+    "id":"Qizy3f7ckW0CHnQErwebd6Gi",
+    "address":{
+      "zip":"93236",
+      "streetName":"Graciela Stravenue",
+      "coordinates":{"lat":"-7.1114","lng":"125.6145"}
+    },
+    "owner":"Preston"
+  },
+  {
+    "id":"w5X4oXzunHiFyf5sfdL8lBHm",
+    "address":{
+      "zip":"46174",
+      "streetName":"Olson Way",
+      "coordinates":{"lat":"59.3656","lng":"171.5258"}
+    },
+    "owner":"Jacinto"
+  },
+  {
+    "id":"DjqtHutF7zsY4URW85AaPEGH",
+    "address":{
+      "zip":"31816",
+      "streetName":"Frami Village",
+      "coordinates":{"lat":"41.6608","lng":"-55.1643"}
+    },
+    "owner":"Mazie"
+  },
+  {
+    "id":"fqDyDsMXqsqkNwCzhJcZRIeH",
+    "address":{
+      "zip":"93418",
+      "streetName":"Cassandra Cape",
+      "coordinates":{"lat":"-26.9248","lng":"117.6831"}
+    },
+    "owner":"Yesenia"
+  },
+  {
+    "id":"DEJqrMuh4cO3M5urTlbgCGzn",
+    "address":{
+      "zip":"57853-5066",
+      "streetName":"Mann Mission",
+      "coordinates":{"lat":"3.3978","lng":"27.9507"}
+    },
+    "owner":"Shanie"
+  },
+  {
+    "id":"YTbF1PtgFVBD8gOIdJ30fodh",
+    "address":{
+      "zip":"29063-4385",
+      "streetName":"Frederique Views",
+      "coordinates":{"lat":"9.8973","lng":"-149.5927"}
+    },
+    "owner":"Ada"
+  },
+  {
+    "id":"Ia6HQ8WGPOcLALLzarWSbY20",
+    "address":{
+      "zip":"45391",
+      "streetName":"Rosendo Drive",
+      "coordinates":{"lat":"33.4622","lng":"-78.1571"}
+    },
+    "owner":"Dixie"
+  },
+  {
+    "id":"FlLJY3ihdCQsMm1oyDoZWbg1",
+    "address":{
+      "zip":"42715",
+      "streetName":"Koepp Plains",
+      "coordinates":{"lat":"52.3335","lng":"128.4330"}
+    },
+    "owner":"Mozell"
+  },
+  {
+    "id":"z7JtGBrbhvFjQ5kUq49hIYT3",
+    "address":{
+      "zip":"76442",
+      "streetName":"Alexie Expressway",
+      "coordinates":{"lat":"9.3120","lng":"137.2384"}
+    },
+    "owner":"Johan"
+  },
+  {
+    "id":"3g5swMRanJz70Ao5BvAc49eF",
+    "address":{
+      "zip":"59801-8378",
+      "streetName":"Hamill Creek",
+      "coordinates":{"lat":"-43.8417","lng":"-126.5874"}
+    },
+    "owner":"Guy"
+  },
+  {
+    "id":"Mh5Xsvw16zEIrsOmUfndDequ",
+    "address":{
+      "zip":"68939-7539",
+      "streetName":"Erling Underpass",
+      "coordinates":{"lat":"-79.1357","lng":"-24.5010"}
+    },
+    "owner":"Annette"
+  },
+  {
+    "id":"yWeQgD8i3o6UqA7uvnQNcVsr",
+    "address":{
+      "zip":"42451",
+      "streetName":"Forest Walks",
+      "coordinates":{"lat":"-83.2321","lng":"91.2058"}
+    },
+    "owner":"Jamel"
+  },
+  {
+    "id":"UQS58ubmlaDfAodkHH9pADES",
+    "address":{
+      "zip":"12537",
+      "streetName":"Maureen Cliff",
+      "coordinates":{"lat":"82.9609","lng":"25.7326"}
+    },
+    "owner":"Hilma"
+  },
+  {
+    "id":"9hr2KDzqEPgEzOeNHH3OLWKF",
+    "address":{
+      "zip":"33188-0613",
+      "streetName":"Callie River",
+      "coordinates":{"lat":"-77.3901","lng":"120.4351"}
+    },
+    "owner":"Dayana"
+  },
+  {
+    "id":"MTzMVObfgtTiTBMHLwyUGcT8",
+    "address":{
+      "zip":"89787",
+      "streetName":"Ottilie Knoll",
+      "coordinates":{"lat":"32.3481","lng":"20.9539"}
+    },
+    "owner":"Kasandra"
+  },
+  {
+    "id":"kQh3QAPRtDjILS3mbc882eMf",
+    "address":{
+      "zip":"89305-4776",
+      "streetName":"Kutch Summit",
+      "coordinates":{"lat":"43.1914","lng":"-84.1274"}
+    },
+    "owner":"Victoria"
+  },
+  {
+    "id":"MXJcOp08FY2ifziLA8FyHpSA",
+    "address":{
+      "zip":"29807",
+      "streetName":"Alexanne Cove",
+      "coordinates":{"lat":"37.5538","lng":"155.4320"}
+    },
+    "owner":"Noemi"
+  },
+  {
+    "id":"XlTCb807jbsSGraarqn4Mnz2",
+    "address":{
+      "zip":"84313-4388",
+      "streetName":"Houston Mission",
+      "coordinates":{"lat":"-40.3816","lng":"146.8190"}
+    },
+    "owner":"Ona"
+  },
+  {
+    "id":"yKgowXN8qpvNO2OrlYg0VE4k",
+    "address":{
+      "zip":"00158",
+      "streetName":"Leffler Burg",
+      "coordinates":{"lat":"-79.3893","lng":"25.8060"}
+    },
+    "owner":"Reyes"
+  },
+  {
+    "id":"k9ywEPsw74MVBAqE2wevmoOB",
+    "address":{
+      "zip":"16277-1839",
+      "streetName":"Vernice Row",
+      "coordinates":{"lat":"9.2643","lng":"62.6631"}
+    },
+    "owner":"Edwina"
+  },
+  {
+    "id":"OzDLtmHD5NW40zsG0VgIp5ye",
+    "address":{
+      "zip":"79391-3640",
+      "streetName":"Mohr Underpass",
+      "coordinates":{"lat":"-34.6558","lng":"53.1878"}
+    },
+    "owner":"Brigitte"
+  },
+  {
+    "id":"UvpMElFepkVq5SYUlv19sFAF",
+    "address":{
+      "zip":"57858-2360",
+      "streetName":"Malinda Drives",
+      "coordinates":{"lat":"-31.8551","lng":"85.1917"}
+    },
+    "owner":"Jerod"
+  },
+  {
+    "id":"4RtuWoSZodi3Yw25tIPZ0URh",
+    "address":{
+      "zip":"83560",
+      "streetName":"Gerhard Underpass",
+      "coordinates":{"lat":"19.4453","lng":"23.6571"}
+    },
+    "owner":"Garland"
+  },
+  {
+    "id":"Qo1yw8RW3ghLwz9bfAn4jJjR",
+    "address":{
+      "zip":"85079",
+      "streetName":"Boris Point",
+      "coordinates":{"lat":"30.1066","lng":"-161.2441"}
+    },
+    "owner":"Giles"
+  },
+  {
+    "id":"BgIKoeZeoCuifwZymUyRdypd",
+    "address":{
+      "zip":"21904",
+      "streetName":"Rau Springs",
+      "coordinates":{"lat":"66.4692","lng":"87.1370"}
+    },
+    "owner":"Maribel"
+  }
+]
--- back-flask/main.py
+++ back-flask/main.py
@@ -1,21 +1,34 @@
-from flask import Flask
+from flask import Flask, Response
+import json
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 import pymongo
 import os
 app = Flask(__name__)
+# import requesters 
+# import requests
+# import volunteers
+# import shop_owners
+# import shops
+
+# print(requesters.requesters)
 
 # MongoDB Database connection
 # Databse name, shopme_db
 
-MONGODB_URI = 'mongodb+srv://asabeneh:Asab123123123@cluster0.8zud1.mongodb.net/shopme_db?retryWrites=true&w=majority'
+MONGODB_URI = 'mongodb+srv://asabeneh:Asab123123123@shopme.azwx2.mongodb.net/shop4me_db?retryWrites=true&w=majority'
 
 client = pymongo.MongoClient(MONGODB_URI)
-db = client.shopme_db
+db = client.shop4me_db
 
 
+# for vol in volunteers.volunteers:
+#     db.volunteers.insert_one(vol)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 
 if __name__ == '__main__':
