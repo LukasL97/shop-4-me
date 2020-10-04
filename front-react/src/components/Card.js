ce294083a9786e54ec69fba0471b644916002d8a
Asabeneh <asabeneh@gmail.com>
2020-10-04 16:04:46 +0300

    box shadow added

--- front-react/src/components/Card.js
+++ front-react/src/components/Card.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Card = ({ item: { name, price }, image }) => {
+  const [visibility, setVisibility] = useState(false)
   return (
     <div className='card'>
       <div className='card-image'>
         <img src={image} alt='' />
       </div>
-      <h2>{name}</h2>
       <div>
-        <small>{price/100}</small>
+        <h2>{name}</h2>
       </div>
-      <div className='icons'>
-        <i class='fas fa-thumbs-up'></i>
-        <i class='fas fa-thumbs-down'></i>
-        <i class='fas fa-star'></i>
+
+      <div className='card-footer'>
+        <div className='icons show'>
+          <i class='fas fa-thumbs-up'></i>
+          <i class='fas fa-thumbs-down'></i>
+          <i class='fas fa-star'></i>
+        </div>
+        <div>
+          <small>{price / 100}</small>
+        </div>
       </div>
     </div>
   )
