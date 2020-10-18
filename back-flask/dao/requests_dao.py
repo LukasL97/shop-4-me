f420721acd9e057b47b30f2f8b3e922d4dcb7df6
Lukas Lehmann <lukas.lehmann97@gmail.com>
2020-10-18 13:25:50 +0300

    46 -- add data access functionality to requests

--- back-flask/dao/requests_dao.py
+++ back-flask/dao/requests_dao.py
@@ -4,6 +4,7 @@ from bson import ObjectId
 from pymongo.database import Database
 
 from dao.abstract_dao import AbstractDAO
+from model.request_status import RequestStatus
 
 
 class RequestsDAO(AbstractDAO):
@@ -13,3 +14,15 @@ class RequestsDAO(AbstractDAO):
 
     def get_requests_by_requester(self, requester_id: str) -> List[Dict[str, Any]]:
         return list(self.collection.find({'requester': ObjectId(requester_id)}))
+
+    def get_requests_by_volunteer(self, volunteer_id: str) -> List[Dict[str, Any]]:
+        return list(self.collection.find({'volunteer': ObjectId(volunteer_id)}))
+
+    def get_open_requests(self) -> List[Dict[str, Any]]:
+        return list(self.collection.find({'status': RequestStatus.SUBMITTED}))
+
+    def update_status(self, id: str, status: int) -> None:
+        self.collection.update_one({'_id': ObjectId(id)}, {'$set': {'status': status}})
+
+    def update_volunteer_and_status(self, id: str, volunteer_id: str, status: int) -> None:
+        self.collection.update_one({'_id': ObjectId(id)}, {'$set': {'volunteer': ObjectId(volunteer_id), 'status': status}})
