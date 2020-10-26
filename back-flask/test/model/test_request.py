a286a24826f01c6e3abbf696d38a8636a611b162
Lukas Lehmann <lukas.lehmann97@gmail.com>
2020-10-26 18:52:25 +0200

    47 -- add tests covering new functionality

--- back-flask/test/model/test_request.py
+++ back-flask/test/model/test_request.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import Mock
 
 from dao.items_dao import ItemsDAO
 from dao.requests_dao import RequestsDAO
 from model.exception import UserSessionIdNotFoundError, ObjectIdNotFoundError, UnauthorizedAccessError, \
-    UnexpectedRequestStatusError
+    UnexpectedRequestStatusError, MissingDeliveryAddressError
 from model.item import Item, ItemHandler
+from model.location.address import AddressHandler, Address
 from model.request import RequestHandler, Request
 from model.request_status import RequestStatus
 from model.user import Requester, Volunteer, ShopOwnerHandler, RequesterHandler, VolunteerHandler
+from test.model.util.stubs import AddressLocatorStub
 from test.mongodb_integration_test_setup import get_empty_local_test_db
 
 
@@ -20,7 +23,9 @@ class RequestHandlerTest(TestCase):
     item_handler = ItemHandler(items_dao, ShopOwnerHandler(None))
     requester_handler = RequesterHandler(None, None)
     volunteer_handler = VolunteerHandler(None)
-    request_handler = RequestHandler(requests_dao, item_handler, requester_handler, volunteer_handler)
+    locator = AddressLocatorStub()
+    address_handler = AddressHandler(locator)
+    request_handler = RequestHandler(requests_dao, item_handler, requester_handler, volunteer_handler, address_handler)
 
     def setUp(self):
         self.requests_dao.clear()
@@ -86,10 +91,11 @@ class RequestHandlerTest(TestCase):
                 session_id
             )
 
-    def test_submit_request_correctly(self):
+    def test_submit_request_without_address_correctly(self):
         session_id = 'sessionId'
         requester_id = '5f81ae776db502d353a84fdf'
-        requester = Requester('login', 'pw', 'first', 'last', requester_id)
+        address = self.address_handler(self.locator.valid_street_1, self.locator.valid_zip_1, self.locator.valid_country_1)
+        requester = Requester('login', 'pw', 'first', 'last', requester_id, address)
         self.requester_handler.active_user_sessions[session_id] = requester
         request = Request(
             requester=requester_id,
@@ -101,6 +107,29 @@ class RequestHandlerTest(TestCase):
         self.request_handler.submit_request(request_id, session_id)
         request = self.request_handler.from_db_object(self.requests_dao.get_all()[0])
         self.assertEqual(request.status, RequestStatus.SUBMITTED)
+        self.assertEqual(request.delivery_address, address)
+        self.assertIsNot(request.submission_date, None)
+        self.assertTrue(type(request.submission_date) == datetime)
+
+    def test_submit_request_with_address_correctly(self):
+        session_id = 'sessionId'
+        requester_id = '5f81ae776db502d353a84fdf'
+        address = self.address_handler(self.locator.valid_street_1, self.locator.valid_zip_1, self.locator.valid_country_1)
+        requester = Requester('login', 'pw', 'first', 'last', requester_id)
+        self.requester_handler.active_user_sessions[session_id] = requester
+        request = Request(
+            requester=requester_id,
+            status=RequestStatus.CREATED,
+            items=[],
+            volunteer=None,
+        )
+        request_id = self.requests_dao.store_one(request.to_db_object())
+        self.request_handler.submit_request(request_id, session_id, address.to_db_object())
+        request = self.request_handler.from_db_object(self.requests_dao.get_all()[0])
+        self.assertEqual(request.status, RequestStatus.SUBMITTED)
+        self.assertEqual(request.delivery_address, address)
+        self.assertIsNot(request.submission_date, None)
+        self.assertTrue(type(request.submission_date) == datetime)
 
     def test_submit_request_with_unknown_session_id(self):
         self.requester_handler.active_user_sessions['someSessionId'] = Mock()
@@ -143,10 +172,26 @@ class RequestHandlerTest(TestCase):
             requester=requester_id,
             status=RequestStatus.IN_PROGRESS,
             items=[],
-            volunteer=None
+            volunteer=None,
+            submission_date=datetime.now()
         )
         request_id = self.requests_dao.store_one(request.to_db_object())
         with self.assertRaises(UnexpectedRequestStatusError):
+            self.request_handler.submit_request(request_id, session_id)
+
+    def test_submit_request_with_missing_delivery_address(self):
+        session_id = 'sessionId'
+        requester_id = '5f81ae776db502d353a84fdf'
+        requester = Requester('login', 'pw', 'first', 'last', requester_id)
+        self.requester_handler.active_user_sessions[session_id] = requester
+        request = Request(
+            requester=requester_id,
+            status=RequestStatus.CREATED,
+            items=[],
+            volunteer=None
+        )
+        request_id = self.requests_dao.store_one(request.to_db_object())
+        with self.assertRaises(MissingDeliveryAddressError):
             self.request_handler.submit_request(request_id, session_id)
 
     def test_get_requesters_own_requests_correctly(self):
@@ -173,13 +218,15 @@ class RequestHandlerTest(TestCase):
             requester=requester_id,
             status=RequestStatus.IN_PROGRESS,
             items=[(item, 3)],
-            volunteer='5f81ae776db502d353a84fdf'
+            volunteer='5f81ae776db502d353a84fdf',
+            submission_date=datetime.now()
         )
         request_3 = Request(
             requester=other_requester_id,
             status=RequestStatus.PAID,
             items=[(item, 1)],
-            volunteer='5f81ae776db502d353a84fdf'
+            volunteer='5f81ae776db502d353a84fdf',
+            submission_date=datetime.now()
         )
         request_1_id = self.requests_dao.store_one(request_1.to_db_object())
         request_2_id = self.requests_dao.store_one(request_2.to_db_object())
@@ -207,19 +254,66 @@ class RequestHandlerTest(TestCase):
             requester='5f7c2d96e48e242b81178822',
             status=RequestStatus.CREATED,
             items=[],
-            volunteer=None
+            volunteer=None,
+            submission_date=datetime.now()
         )
         request_2 = Request(
             requester='5f7c2d96e48e242b81178822',
             status=RequestStatus.SUBMITTED,
             items=[],
-            volunteer=None
+            volunteer=None,
+            submission_date=datetime.now()
         )
         request_1_id = self.requests_dao.store_one(request_1.to_db_object())
         request_2_id = self.requests_dao.store_one(request_2.to_db_object())
         requests = self.request_handler.get_open_requests(session_id)
         self.assertEqual(len(requests), 1)
         self.assertEqual(requests[0]['id'], request_2_id)
+
+    def test_get_open_requests_with_area_correctly(self):
+        volunteer_point = (60.195382, 24.938703)
+        closest_request_point = (60.195318, 24.940731)
+        second_closest_request_point = (60.193371, 24.943177)
+        out_of_range_request_point = (60.143397, 24.985745)
+        range = 1
+        session_id = 'sessionId'
+        volunteer = Volunteer('login', 'pw', 'first', 'last', 'userId')
+        self.volunteer_handler.active_user_sessions[session_id] = volunteer
+        out_of_range_request_date = datetime.fromisoformat('2020-10-25 13:03:55.792200')
+        second_closest_request_date = datetime.fromisoformat('2020-10-26 13:03:55.792200')
+        closest_request_date = datetime.fromisoformat('2020-10-27 13:03:55.792200')
+        closest_request = Request(
+            requester='5f7c2d96e48e242b81178822',
+            status=RequestStatus.SUBMITTED,
+            items=[],
+            volunteer=None,
+            delivery_address=Address('', '', '', closest_request_point[0], closest_request_point[1]),
+            submission_date=closest_request_date
+        )
+        second_closest_request = Request(
+            requester='5f7c2d96e48e242b81178822',
+            status=RequestStatus.SUBMITTED,
+            items=[],
+            volunteer=None,
+            delivery_address=Address('', '', '', second_closest_request_point[0], second_closest_request_point[1]),
+            submission_date=second_closest_request_date
+        )
+        out_of_range_request = Request(
+            requester='5f7c2d96e48e242b81178822',
+            status=RequestStatus.SUBMITTED,
+            items=[],
+            volunteer=None,
+            delivery_address=Address('', '', '', out_of_range_request_point[0], out_of_range_request_point[1]),
+            submission_date=out_of_range_request_date
+        )
+        closest_request_id = self.requests_dao.store_one(closest_request.to_db_object())
+        second_closest_request_id = self.requests_dao.store_one(second_closest_request.to_db_object())
+        out_of_range_request_id = self.requests_dao.store_one(out_of_range_request.to_db_object())
+        area = {'range': range, 'lat': volunteer_point[0], 'lng': volunteer_point[1]}
+        requests = self.request_handler.get_open_requests(session_id, area)
+        self.assertEqual(len(requests), 2)
+        self.assertEqual(requests[0]['id'], second_closest_request_id)
+        self.assertEqual(requests[1]['id'], closest_request_id)
 
     def test_get_open_requests_with_unknown_session_id(self):
         self.volunteer_handler.active_user_sessions['someSessionId'] = Mock()
@@ -242,13 +336,15 @@ class RequestHandlerTest(TestCase):
             requester='5f81ae776db502d353a84fdf',
             status=RequestStatus.IN_PROGRESS,
             items=[],
-            volunteer=volunteer_id
+            volunteer=volunteer_id,
+            submission_date=datetime.now()
         )
         request_3 = Request(
             requester='5f81ae776db502d353a84fdf',
             status=RequestStatus.IN_PROGRESS,
             items=[],
-            volunteer=other_volunteer_id
+            volunteer=other_volunteer_id,
+            submission_date=datetime.now()
         )
         request_1_id = self.requests_dao.store_one(request_1.to_db_object())
         request_2_id = self.requests_dao.store_one(request_2.to_db_object())
