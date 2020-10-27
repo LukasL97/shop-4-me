d468557ba199bc9b946aa82d4cf3d8bc25e52b66
Walter Grönholm <waltergronholm@gmail.com>
2020-10-27 19:49:01 +0200

    27 - Home page is chosen based on user_type cookie

--- front-react/src/components/Home.js
+++ front-react/src/components/Home.js
@@ -2,11 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from './Layout'
 import Cards from './Cards'
+import Cookies from 'universal-cookie'
+import AvailabilityCalendar from './Calendar'
 
 const Home = (props) => {
+  const cookies = new Cookies();
+  let userType = cookies.get('user_type');
+  let component = userType === "Requester" ?
+	<Cards data={props.data} addItemToCart={props.addItemToCart} /> :
+	userType === "Volunteer" ?
+    <AvailabilityCalendar /> :
+	<Cards data={props.data} addItemToCart={props.addItemToCart} />
+  
   return (
     <Layout>
-      <Cards data={props.data} addItemToCart={props.addItemToCart} />
+      {component}
     </Layout>
   )
 }
