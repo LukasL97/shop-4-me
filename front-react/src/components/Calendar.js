17c6e3ec8ed3595238702a89aed5b9d63dbfef08
Walter Grönholm <waltergronholm@gmail.com>
2020-11-01 20:44:29 +0200

    27 -- Fixed infinite load bug

--- front-react/src/components/Calendar.js
+++ front-react/src/components/Calendar.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, { render } from '@fullcalendar/react'
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import Cookies from 'universal-cookie'
@@ -33,63 +33,71 @@ const Calendar = (props) => {
   </div>
 }
 
-const PopulateCalendar = (setCalendarData) => {
-	const cookies = new Cookies();
+class AvailabilityCalendar extends Component {
+	state = {
+		calendarData: [],
+		thanks: {weekly: 42, monthly: 123, yearly: 123}
+	}
+  
+	componentDidMount() {
+		this.populateCalendar()
+	}
 
-	const url = 'http://localhost:5000/timeframes'
-	axios({
-		method: "POST",
-		url: url, 
-		data: {
-			sessionId: cookies.get("access_token")
+	populateCalendar() {
+		const cookies = new Cookies();
+	
+		const url = 'http://localhost:5000/timeframes'
+		axios({
+			method: "POST",
+			url: url, 
+			data: {
+				sessionId: cookies.get("access_token")
+			}
+		}).then((response) => {
+			var now = new Date();
+	
+			const calendarData = response.data.map((timeframe) => ({
+				...timeframe,
+				nrequests: (timeframe.requests || []).length()
+			})).map((timeframe) => ({
+				start: timeframe.start,
+				end: timeframe.end,
+				editable: new Date(timeframe.start) >= now,
+				title: new Date(timeframe.end) < now ? 
+					(timeframe.nrequests == 0 ? 
+					"Skipped" :
+					"You helped " + timeframe.nrequests + " household" + (timeframe.nrequests == 1 ? "!" : "s!")) :
+				(new Date(timeframe.start) < now ?
+					"Delivering" :
+					"Available"),
+				backgroundColor: new Date(timeframe.end) < now ? "#474" : (new Date(timeframe.start) < now ? "#7F7" : "#77F")
+			}))
+			
+			this.state.calendarData = calendarData
+		})
+	}
+
+	render() {
+		var thanks = this.state.thanks;
+		var thanksComponent;
+		if (thanks.weekly > 0) {
+			thanksComponent = <AppreciationHeader thanks={thanks.weekly} intervalName={"week"}/>
+		} else if (thanks.monthly > 0) {
+			thanksComponent = <AppreciationHeader thanks={thanks.monthly} intervalName={"month"}/>
+		} else if (thanks.yearly > 0) {
+			thanksComponent = <AppreciationHeader thanks={thanks.yearly} intervalName={"year"}/>
+		} else {
+			thanksComponent = <AppreciationHeader />
 		}
-	}).then((response) => {
-		var now = new Date();
-
-		const calendarData = response.data.map((timeframe) => ({
-			...timeframe,
-			nrequests: (timeframe.requests || []).length()
-		})).map((timeframe) => ({
-			start: timeframe.start,
-			end: timeframe.end,
-			editable: new Date(timeframe.start) >= now,
-			title: new Date(timeframe.end) < now ? 
-				(timeframe.nrequests == 0 ? 
-				"Skipped" :
-				"You helped " + timeframe.nrequests + " household" + (timeframe.nrequests == 1 ? "!" : "s!")) :
-			(new Date(timeframe.start) < now ?
-				"Delivering" :
-				"Available"),
-			backgroundColor: new Date(timeframe.end) < now ? "#474" : (new Date(timeframe.start) < now ? "#7F7" : "#77F")
-		}))
-		setCalendarData(calendarData)
-	})
-}
-
-const AvailabilityCalendar = (props) => {
-  var [calendarData, setCalendarData] = useState([])
-
-  PopulateCalendar(setCalendarData)
-
-  var thanks = {weekly: 42, monthly: 123, yearly: 123}
-  var thanksComponent;
-  if (thanks.weekly > 0) {
-    thanksComponent = <AppreciationHeader thanks={thanks.weekly} intervalName={"week"}/>
-  } else if (thanks.monthly > 0) {
-    thanksComponent = <AppreciationHeader thanks={thanks.monthly} intervalName={"month"}/>
-  } else if (thanks.yearly > 0) {
-    thanksComponent = <AppreciationHeader thanks={thanks.yearly} intervalName={"year"}/>
-  } else {
-    thanksComponent = <AppreciationHeader />
-  }
-  return <>
-    {thanksComponent}
-    <Calendar
-      from={"2020-10-20"}
-      to={"2020-10-30"}
-      data={calendarData}
-    />
-  </>
+		return <>
+			{thanksComponent}
+			<Calendar
+			from={"2020-10-20"}
+			to={"2020-10-30"}
+			data={this.state.calendarData}
+			/>
+		</>
+	}
 }
 
 const AppreciationHeader = (props) => {
